3c071807bd84d859400bf6f2c6ecc208a3a13a66
灰级计萌 <78867289+YISHENGPINGAN51@users.noreply.github.com>
2022-04-04 10:19:02 +0800

    江科大 3-5

---
+++ 3-5 光敏传感器控制蜂鸣器/User/main.c
@@ -0,0 +1,24 @@
+#include "stm32f10x.h"                  // Device header
+#include "Delay.h"
+#include "Buzzer.h"
+#include "LightSensor.h"
+
+uint8_t KeyNum;
+
+int main(void)
+{
+	Buzzer_Init();
+	LightSensor_Init();
+	
+	while (1)
+	{
+		if (LightSensor_Get() == 0)
+		{
+			Buzzer_ON();
+		}
+		else
+		{
+			Buzzer_OFF();
+		}
+	}
+}
